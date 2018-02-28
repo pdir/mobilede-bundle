b01850d27d3ecab4b87e1bae8375e71db9d5ae64
Philipp Seibt <seibt@pdir.de>
2018-02-28 18:05:18 +0100

    update template and languages file

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * mobilede for Contao Open Source CMS
+ *
+ * Copyright (C) 2017 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    mobilede
+ * @link       https://pdir.de/mobilede
+ * @license    pdir license - All-rights-reserved - commercial extension
+ * @author     pdir GmbH <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Label translation
+ */
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['airbag'] = 'Airbag';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['climatisation'] = 'Klimatisierung';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['door-count'] = 'Anzahl der Türen';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['emission-class'] = 'Schadstoffklasse';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['emission-sticker'] = 'Umweltplakette';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['power'] = 'PS';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['kba'] = '';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['schwacke-code'] = 'Schwacke-Code';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['num-seats'] = 'Anzahl Sitzplätze';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['cubic-capacity'] = 'Hubraum';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['identification-number'] = 'ID';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['interior-color'] = 'Innenfarbe';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['interior-type'] = 'Innenausstattung';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['countryversion'] = '';
+$GLOBALS['TL_LANG']['pdirMobileDe']['field_keys']['parking-assistants'] = 'Parkassistent';
