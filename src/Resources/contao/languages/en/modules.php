76a414acd8146daded922f0d98824d9189649110
Philipp Seibt <seibt@muesliplus.de>
2018-04-23 16:51:22 +0200

    begin translations

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * mobilede for Contao Open Source CMS
+ *
+ * Copyright (C) 2017 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    mobilede
+ * @link       https://pdir.de/mobilede
+ * @license    pdir license - All-rights-reserved - commercial extension
+ * @author     pdir GmbH <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Module translation
+ */
+
+$GLOBALS['TL_LANG']['MOD']['pdir'][0] = "pdir Apps";
+$GLOBALS['TL_LANG']['MOD']['mobileDeSetup'][0]   = "Mobile.de Setup";
+$GLOBALS['TL_LANG']['MOD']['mobileDeSetup'][1]   = "Here you can manage the Mobile.de App";
+$GLOBALS['TL_LANG']['CTE']['mobileDeList'][0]   = "Mobile.de List View";
+$GLOBALS['TL_LANG']['CTE']['mobileDeList'][1]   = "Content element to place the Mobile.de list view on the desired page.";
+$GLOBALS['TL_LANG']['CTE']['mobileDeReader'][0]   = "Mobile.de Detail View";
+$GLOBALS['TL_LANG']['CTE']['mobileDeReader'][1]   = "Content element to place the Mobile.de detail view on the desired page.";
+
+$GLOBALS['TL_LANG']['MOBILEDE']['emailBody'] = "<br>Hello pdir Team,<br><br>wir haben die mobile.de Schnittstelle für Contao 4 installiert, getestet und würden diese gerne erwerben und für unseren Kunden einsetzten.<br><br>Die Domain lautet: [HIER DOMAIN ANGEBEN] <br><br>Die Rechnung soll auf folgende Adresse ausgestellt werden:<br><br>[HIER RECHNUNGSANSCHRIFT ANGEBEN]<br><br>Die Serverdaten aus dem Backend lauten wie folgt:<br>IP: :IP:<br>Hostname: :HOST: <br>Domain: :DOMAIN: <br><br>Mit freundlichen Grüßen<br>";
