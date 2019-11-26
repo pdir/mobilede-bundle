294dcd95a3ee74824a22220305b6cc1a53c36470
Mathias Arzberger <develop@pdir.de>
2019-11-26 12:41:45 +0100

    update version

--- src/Module/MobileDeSetup.php
+++ src/Module/MobileDeSetup.php
@@ -21,7 +21,7 @@ class MobileDeSetup extends \BackendModule
     /**
      * mobilede version.
      */
-    const VERSION = '2.5.2';
+    const VERSION = '2.5.3';
 
     /**
      * Extension mode.
