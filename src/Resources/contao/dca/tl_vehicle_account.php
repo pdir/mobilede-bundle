93aec3d20493a2ef6e6d43298418b9ab458acd82
Mathias <develop@pdir.de>
2021-05-25 12:41:10 +0200

    refactor for vehicle account

--- src/Resources/contao/dca/tl_vehicle_account.php
+++ src/Resources/contao/dca/tl_vehicle_account.php
@@ -101,7 +101,8 @@ $GLOBALS['TL_DCA'][$strTable] = [
             'exclude' => true,
             'inputType' => 'select',
             'options' => $GLOBALS['TL_LANG'][$strTable]['apiTypeOptions'],
-            'eval' => ['includeBlankOption' => true, 'tl_class' => 'w50']
+            'eval' => ['includeBlankOption' => true, 'tl_class' => 'w50'],
+            'sql' => "varchar(64) NOT NULL default ''",
         ],
         'api_explanation' => [
             'exclude' => true,
@@ -116,16 +117,19 @@ $GLOBALS['TL_DCA'][$strTable] = [
             'exclude' => true,
             'inputType' => 'text',
             'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50', 'preserveTags' => true],
+            'sql' => "varchar(64) NOT NULL default ''",
         ],
         'api_user_secret' => [
             'exclude' => true,
             'inputType' => 'text',
             'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50', 'preserveTags' => true],
+            'sql' => "varchar(64) NOT NULL default ''",
         ],
         'mobilede_customer_number' => [
             'exclude' => true,
             'inputType' => 'text',
             'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50', 'preserveTags' => true],
+            'sql' => "varchar(64) NOT NULL default ''",
         ],
         'enabled' => [
             'exclude' => true,
@@ -137,6 +141,7 @@ $GLOBALS['TL_DCA'][$strTable] = [
                     return '1' === $value;
                 },
             ],
+            'sql' => "char(1) NOT NULL default ''",
         ],
     ],
 ];
